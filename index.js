9b29052241be216d9e66c013cc7524afbbc79743
otegaa <ekiugbotega@gmail.com>
2023-03-15 16:06:41 +0100

    Fix linters issue

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const openMenu = document.querySelector(".hamburger-container__img");
-const closeMenu = document.querySelector(".hamburger__close-img");
-const hamburger = document.querySelector(".hamburger");
-const menuLinks = document.querySelectorAll(".menu-links");
+const openMenu = document.querySelector('.hamburger-container__img');
+const closeMenu = document.querySelector('.hamburger__close-img');
+const hamburger = document.querySelector('.hamburger');
+const menuLinks = document.querySelectorAll('.menu-links');
 const featuredSpeakers = document.querySelector(
-  ".featured-speakers__dynamic-box"
+  '.featured-speakers__dynamic-box',
 );
-const seeMore = document.querySelector(".see-more__btn");
-const seeLess = document.querySelector(".see-less");
+const seeMore = document.querySelector('.see-more__btn');
+const seeLess = document.querySelector('.see-less');
 
 // Opening the hamburger menu
 function openHamburgerMenu() {
-  hamburger.classList.remove("hidden");
+  hamburger.classList.remove('hidden');
 }
 
-openMenu.addEventListener("click", openHamburgerMenu);
+openMenu.addEventListener('click', openHamburgerMenu);
 
 // Closing the hamburger menu
 
 function closeHamburgerMenu() {
-  hamburger.classList.add("hidden");
+  hamburger.classList.add('hidden');
 }
 
-closeMenu.addEventListener("click", closeHamburgerMenu);
+closeMenu.addEventListener('click', closeHamburgerMenu);
 
 // Go to the link
 menuLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    hamburger.classList.add("hidden");
+  link.addEventListener('click', () => {
+    hamburger.classList.add('hidden');
   });
 });
 
@@ -34,51 +34,51 @@ menuLinks.forEach((link) => {
 const dynamicSpeakers = [
   {
     id: 1,
-    img: "/assets/Chimamanda.svg",
-    name: "Chimamanda Ngozi Adichie",
-    occupation: " Author and Publisher",
+    img: '/assets/Chimamanda.svg',
+    name: 'Chimamanda Ngozi Adichie',
+    occupation: ' Author and Publisher',
     description:
-      "Chimamanda is an award winning writer of numerous books, sold in over 100 languages.",
+      'Chimamanda is an award winning writer of numerous books, sold in over 100 languages.',
   },
   {
     id: 2,
-    img: "/assets/Safra-cat.svg",
-    name: "Safra Catz",
-    occupation: "CEO Oracle",
+    img: '/assets/Safra-cat.svg',
+    name: 'Safra Catz',
+    occupation: 'CEO Oracle',
     description:
-      " Safra is the CEO of Oracle,a banker and an accounting lecturer at Stanford Business School",
+      ' Safra is the CEO of Oracle,a banker and an accounting lecturer at Stanford Business School',
   },
   {
     id: 3,
-    img: "/assets/Sumbul.svg",
-    name: "Dr. Sumbul Desaie",
-    occupation: "Vice president of health, Apple",
+    img: '/assets/Sumbul.svg',
+    name: 'Dr. Sumbul Desaie',
+    occupation: 'Vice president of health, Apple',
     description:
       "Desaie has spear-headed Apple's effort to break into the healthcare market through fresh technologies.",
   },
   {
     id: 4,
-    img: "/assets/Ifedayo.svg",
-    name: "Ifedayo Agoro",
-    occupation: "CEO DANG!, content and media strategist",
+    img: '/assets/Ifedayo.svg',
+    name: 'Ifedayo Agoro',
+    occupation: 'CEO DANG!, content and media strategist',
     description:
-      "Ife owns an online community that inspires women to lead better lives. She is the founder and creative lead of DANG!.",
+      'Ife owns an online community that inspires women to lead better lives. She is the founder and creative lead of DANG!.',
   },
   {
     id: 5,
-    img: "/assets/Fei-fei.svg",
-    name: "Fei-Fei LI",
+    img: '/assets/Fei-fei.svg',
+    name: 'Fei-Fei LI',
     occupation: " Co-director of Stanford's Human-Centered AI Institute",
     description:
-      "Fei-Fei co-founded AI4ALL, and has helped increased the diversity in the AI sphere through educating and mentoring people.",
+      'Fei-Fei co-founded AI4ALL, and has helped increased the diversity in the AI sphere through educating and mentoring people.',
   },
   {
     id: 6,
-    img: "/assets/Chimamanda.svg",
-    name: "Blessing Ogodo",
-    occupation: " UI/UX Designer",
+    img: '/assets/Chimamanda.svg',
+    name: 'Blessing Ogodo',
+    occupation: ' UI/UX Designer',
     description:
-      "Blessing is one of the fastest rising stars in tech. She has been a major part of the progress of many companies in Africa and Europe.",
+      'Blessing is one of the fastest rising stars in tech. She has been a major part of the progress of many companies in Africa and Europe.',
   },
 ];
 
@@ -106,48 +106,48 @@ const speakers = function () {
   }
 };
 
-window.addEventListener("load", speakers);
+window.addEventListener('load', speakers);
 
 // See more button
 let currentItems = 2;
 
-seeMore.addEventListener("click", () => {
-  const speakers = [...document.querySelectorAll(".featured-speakers__box")];
+seeMore.addEventListener('click', () => {
+  const speakers = [...document.querySelectorAll('.featured-speakers__box')];
 
   for (let i = currentItems; i < speakers.length; i += 1) {
-    speakers[i].style.display = "flex";
+    speakers[i].style.display = 'flex';
   }
 
   currentItems += 4;
 
   if (currentItems >= speakers.length) {
-    seeMore.style.display = "none";
-    seeLess.classList.remove("hidden");
+    seeMore.style.display = 'none';
+    seeLess.classList.remove('hidden');
   }
 });
 
 // see less button
-seeLess.addEventListener("click", () => {
-  const speakers = [...document.querySelectorAll(".featured-speakers__box")];
+seeLess.addEventListener('click', () => {
+  const speakers = [...document.querySelectorAll('.featured-speakers__box')];
 
   for (let i = 2; i < speakers.length; i += 1) {
-    speakers[i].style.display = "none";
+    speakers[i].style.display = 'none';
   }
 
   currentItems -= 4;
 
   if (currentItems <= 2) {
-    seeMore.style.display = "flex";
-    seeLess.classList.add("hidden");
+    seeMore.style.display = 'flex';
+    seeLess.classList.add('hidden');
   }
 });
 
 // Active page
 const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll(".active");
+const navLinks = document.querySelectorAll('.active');
 
 navLinks.forEach((link) => {
   if (link.href.includes(`${activePage}`)) {
-    link.classList.add("active");
+    link.classList.add('active');
   }
 });
