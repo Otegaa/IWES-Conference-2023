d24a1890189a175d377e429f29beadcbe32eb96e
Shpetim Aliu <106196770+shpetimaliu@users.noreply.github.com>
2023-05-13 02:59:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,82 @@
-const openMenu = document.querySelector('.hamburger-container__img');
-const closeMenu = document.querySelector('.hamburger__close-img');
-const hamburger = document.querySelector('.hamburger');
-const menuLinks = document.querySelectorAll('.menu-links');
-const featuredSpeakers = document.querySelector(
-  '.featured-speakers__dynamic-box',
-);
-const seeMore = document.querySelector('.see-more__btn');
-const seeLess = document.querySelector('.see-less');
+const hamburger = {
+  container: document.querySelector('.hamburger-container__img'),
+  close: document.querySelector('.hamburger__close-img'),
+  menu: document.querySelector('.hamburger'),
+  menuLinks: document.querySelectorAll('.menu-links'),
+};
 
-// Opening the hamburger menu
-function openHamburgerMenu() {
-  hamburger.classList.remove('hidden');
-}
+const speakers = {
+  container: document.querySelector('.featured-speakers__dynamic-box'),
+  dynamicSpeakers: [
+    {
+      id: 1,
+      img: './assets/Chimamanda.svg',
+      name: 'Chimamanda Ngozi Adichie',
+      occupation: ' Author and Publisher',
+      description:
+        'Chimamanda is an award winning writer of numerous books, sold in over 100 languages.',
+    },
+    {
+      id: 2,
+      img: './assets/Safra-cat.svg',
+      name: 'Safra Catz',
+      occupation: 'CEO Oracle',
+      description:
+        ' Safra is the CEO of Oracle,a banker and an accounting lecturer at Stanford Business School',
+    },
+    {
+      id: 3,
+      img: './assets/Sumbul.svg',
+      name: 'Dr. Sumbul Desaie',
+      occupation: 'Vice president of health, Apple',
+      description:
+        "Desaie has spear-headed Apple's effort to break into the healthcare market through fresh technologies.",
+    },
+    {
+      id: 4,
+      img: './assets/Ifedayo.svg',
+      name: 'Ifedayo Agoro',
+      occupation: 'CEO DANG!, content and media strategist',
+      description:
+        'Ife owns an online community that inspires women to lead better lives. She is the founder and creative lead of DANG!.',
+    },
+    {
+      id: 5,
+      img: './assets/Fei-fei.svg',
+      name: 'Fei-Fei LI',
+      occupation: " Co-director of Stanford's Human-Centered AI Institute",
+      description:
+        'Fei-Fei co-founded AI4ALL, and has helped increased the diversity in the AI sphere through educating and mentoring people.',
+    },
+    {
+      id: 6,
+      img: './assets/Chimamanda.svg',
+      name: 'Blessing Ogodo',
+      occupation: ' UI/UX Designer',
+      description:
+        'Blessing is one of the fastest rising stars in tech. She has been a major part of the progress of many companies in Africa and Europe.',
+    },
+  ],
+  currentItems: 2,
+  seeMore: document.querySelector('.see-more__btn'),
+  seeLess: document.querySelector('.see-less'),
+};
 
-openMenu.addEventListener('click', openHamburgerMenu);
+// Opening and Closing the hamburger menu
+const openHamburgerMenu = () => {
+  hamburger.menu.classList.remove('hidden');
+};
 
-// Closing the hamburger menu
+const closeHamburgerMenu = () => {
+  hamburger.menu.classList.add('hidden');
+};
 
-function closeHamburgerMenu() {
-  hamburger.classList.add('hidden');
-}
-
-closeMenu.addEventListener('click', closeHamburgerMenu);
+hamburger.container.addEventListener('click', openHamburgerMenu);
+hamburger.close.addEventListener('click', closeHamburgerMenu);
 
 // Go to the link
-menuLinks.forEach((link) => {
-  link.addEventListener('click', () => {
-    hamburger.classList.add('hidden');
-  });
+hamburger.menuLinks.forEach((link) => {
+  link.addEventListener('click', closeHamburgerMenu);
 });
 
 // Dynamic speakers
